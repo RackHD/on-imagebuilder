4e957c7bcab2f5f7b99bfa30daaf32deed1cd617
Davidjohn Blodgett <davidjohn.blodgett@emc.com>
2016-02-12 16:05:53 -0800

    Implemented wrapper shell scripts to replace Ansible playbooks in order to support Ansible 2.0
    squashed commits
    
    added set and comments, rm old yml files
    
    added example.sh
    
    updated readme
    
    more updates to readme
    
    some more updates to readme
    
    readme and wrapper updates and travis bumped to 2.0 install
    
    fixing travis
    
    fixing path issue
    
    cleanup after rebase
    
    travis edit
    
    added version to travis ansible install
    
    use pip for ansible

---
+++ build_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# build initrd
+sudo ansible-playbook -i hosts common/initrd_wrapper.yml -e "config_file=vars/initrd.yml provisioner=roles/initrd/provision_initrd"
+
+# build basefs
+sudo ansible-playbook -i hosts common/basefs_wrapper.yml -e "config_file=vars/basefs.yml provisioner=roles/basefs/provision_rootfs"
+
+# build discovery overlay
+sudo ansible-playbook -i hosts common/overlay_wrapper.yml -e "config_file=vars/discovery_overlay.yml provisioner=roles/overlay/provision_discovery_overlay"
+
+# build ipxe
+sudo ansible-playbook -i hosts common/ipxe_builder.yml -e "config_file=vars/ipxe.yml"
+
+# syslinx ipxe
+sudo ansible-playbook -i hosts common/syslinux_builder.yml -e "config_file=vars/syslinux.yml"
---
+++ build_oem.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# intel overlay
+sudo ansible-playbook -i hosts common/overlay_wrapper.yml -e "config_file=vars/oem/intel.yml provisioner=roles/oem/overlay/provision_intel_flashing_overlay"
+
+# quanta overlay
+sudo ansible-playbook -i hosts common/overlay_wrapper.yml -e "config_file=vars/oem/quanta.yml provisioner=roles/oem/overlay/provision_quanta_flashing_overlay"
