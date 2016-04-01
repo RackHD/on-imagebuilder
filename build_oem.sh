b522fbef8c446a1c2e4bd267422e6a017e31f97c
Krein.Peng <zuiliang.peng@emc.com>
2016-04-01 15:40:09 +0800

    Update build_oem.sh to include Dell overlay command, update desciption about perccli tool

--- build_oem.sh
+++ build_oem.sh
@@ -6,3 +6,6 @@ sudo ansible-playbook -i hosts common/overlay_wrapper.yml -e "config_file=vars/o
 
 # quanta overlay
 sudo ansible-playbook -i hosts common/overlay_wrapper.yml -e "config_file=vars/oem/quanta.yml provisioner=roles/oem/overlay/provision_quanta_flashing_overlay"
+
+# dell overlay
+sudo ansible-playbook -i hosts common/overlay_wrapper.yml -e "config_file=vars/oem/dell_raid_overlay.yml provisioner=roles/oem/overlay/provision_dell_raid_overlay"
