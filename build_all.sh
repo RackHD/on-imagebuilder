c3168c17e670bab35156307c805739b8d3061462
keedya <andre.keedy@emc.com>
2017-09-18 14:47:36 -0400

    Remove microkernel build from build_all.sh
    - Currently we still need old mikrokernel image for other workflows like shell-command
      in this commit, we download the microkernel from bintray instead of building it locally

--- build_all.sh
+++ build_all.sh
@@ -2,16 +2,16 @@
 set -e
 
 # build initrd
-sudo ansible-playbook -i hosts common/initrd_wrapper.yml -e "config_file=vars/initrd.yml provisioner=roles/initrd/provision_initrd"
+#sudo ansible-playbook -i hosts common/initrd_wrapper.yml -e "config_file=vars/initrd.yml provisioner=roles/initrd/provision_initrd"
 
 # build minbasefs
-sudo ansible-playbook -i hosts common/basefs_wrapper.yml -e "config_file=vars/basefs.yml provisioner=roles/basefs/provision_rootfs"
+#sudo ansible-playbook -i hosts common/basefs_wrapper.yml -e "config_file=vars/basefs.yml provisioner=roles/basefs/provision_rootfs"
 
 # build full basefs
-sudo ansible-playbook -i hosts common/basefs_wrapper.yml -e "config_file=vars/basefs-full.yml provisioner=roles/basefs/provision_rootfs"
+#sudo ansible-playbook -i hosts common/basefs_wrapper.yml -e "config_file=vars/basefs-full.yml provisioner=roles/basefs/provision_rootfs"
 
 # build discovery overlay
-sudo ansible-playbook -i hosts common/overlay_wrapper.yml -e "config_file=vars/discovery_overlay.yml provisioner=roles/overlay/provision_discovery_overlay"
+#sudo ansible-playbook -i hosts common/overlay_wrapper.yml -e "config_file=vars/discovery_overlay.yml provisioner=roles/overlay/provision_discovery_overlay"
 
 # build micro-docker
 sudo ansible-playbook -i hosts common/docker_builder.yml
